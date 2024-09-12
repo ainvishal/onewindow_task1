845dfecbbf69a4869d78a04b699da0d035e4bfbc
ainvis <109056582+ainvishal@users.noreply.github.com>
2024-09-12 20:47:34 +0530

    components added

---
+++ src/components/Cardcomponent.jsx
@@ -0,0 +1,13 @@
+
+
+export default function Cardcomponent({item}) {
+    return(
+        <>
+            <div className="card">
+                <div className="card-name">
+                    <h1>card :</h1>
+                </div>
+            </div>
+        </>
+    );
+}
